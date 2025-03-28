0df3db5d877257ddacc97da601b2f622397fdffe
Emmanuel Lefevre <waiasiaa@protonmail.com>
2025-03-28 22:49:35 +0100

    add next config for secrets

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  env: {
+    JWT_SECRET: process.env.JWT_SECRET,
+    REFRESH_SECRET: process.env.REFRESH_SECRET
+  },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
