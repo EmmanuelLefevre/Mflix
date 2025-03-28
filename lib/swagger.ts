cbce72a5af9530ced8082a990a4f5bfa7e5b0f7f
Emmanuel Lefevre <waiasiaa@protonmail.com>
2025-03-28 13:48:34 +0100

    add a contact for Swagger documentation

--- lib/swagger.ts
+++ lib/swagger.ts
@@ -10,7 +10,10 @@ export const getApiDocs = async () => {
       info: {
         title: "Mflix Movie list API",
         version: "1.0",
-        description: "Julien Couraud TP"
+        description: "Julien Couraud TP",
+        contact: {
+          email: "julien.couraud@reseau-cd.net",
+        },
       },
       components: {
         securitySchemes: {
