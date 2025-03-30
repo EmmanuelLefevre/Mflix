861a53ae276d3d8b8a250f777f07d1d69f813629
Emmanuel Lefevre <waiasiaa@protonmail.com>
2025-03-30 17:59:26 +0200

    indentation

--- next.config.js
+++ next.config.js
@@ -1,10 +1,12 @@
-/** @type {import('next').NextConfig} */
+/**
+ * @type {import('next').NextConfig}
+ */
 const nextConfig = {
   reactStrictMode: false,
   env: {
     JWT_SECRET: process.env.JWT_SECRET,
     REFRESH_SECRET: process.env.REFRESH_SECRET
-  },
+  }
 };
 
 module.exports = nextConfig;
