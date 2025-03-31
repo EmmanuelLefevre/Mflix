7f52326f75e4ee1b26648c48193122f72db4632e
Emmanuel Lefevre <waiasiaa@protonmail.com>
2025-03-31 19:54:48 +0200

    maj scss

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,15 @@
 import type { Metadata } from "next";
 
 import { Roboto } from "next/font/google";
-import "@/styles/globals.css";
+import "@/public/styles/globals.css";
+import "@/public/styles/global.scss";
 
 const roboto = Roboto({ subsets: ["latin", "latin-ext"] });
 
 export const metadata: Metadata = {
   title: "Mflix",
   authors: [{ name: "Emmanuel Lefevre" }],
-  keywords: ["NextJS", "Tailwind", "MongoDB", "Css", "Vercel"],
+  keywords: ["NextJS", "Sass", "Tailwind", "MongoDB", "Vercel"],
   description: "🎞️ Movies list Next.js API"
 };
 
--- pages/login.tsx
+++ pages/login.tsx
@@ -1,16 +1,29 @@
 import { useState } from "react";
 import { useRouter } from "next/router";
 
+import "@/public/styles/global.scss";
+
 
 const LoginPage = () => {
   const router = useRouter();
 
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [, setError] = useState<string | null>(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error , setError] = useState<string | null>(null);
+  const [emailError, setEmailError] = useState<string | null>(null);
+
+  const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+  const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+    setEmail(value);
+    setEmailError(emailRegex.test(value) ? null : "Format d'email invalide !");
+  };
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
+    setIsLoading(true);
 
     try {
       const response = await fetch("/api/auth/login", {
@@ -23,42 +36,63 @@ const LoginPage = () => {
 
       if (!response.ok) {
         const data = await response.json();
-        setError(data.error || "Login failed");
+        setError(data.error || "Échec de la connexion !");
+
         return;
       }
 
       router.push("/api-doc");
     }
     catch (err) {
-      setError("Unexpected error occurred");
+      setError("Une erreur inattendue est survenue !");
+    }
+    finally {
+      setIsLoading(false);
     }
   };
 
   return (
-    <div id="login-form">
-      <h1>Login</h1>
-      <form onSubmit={ handleSubmit }>
-        <label>Email</label>
-        <input
-          type="email"
-          placeholder="neo@matrix.com"
-          value={ email }
-          onChange={(e) => setEmail(e.target.value)}
-          required/>
-        <label>Password</label>
-        <input
-          type="password"
-          placeholder="Matrix1999!"
-          value={ password }
-          onChange={(e) => setPassword(e.target.value)}
-          required/>
-        <div id="submit-button">
-          <button type="submit">Login</button>
-        </div>
-      </form>
-    </div>
+    <main>
+      <div id="login-form">
+        <h1>Login</h1>
+        <form onSubmit={ handleSubmit }>
+
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            name="email"
+            value={ email }
+            onChange={ handleEmailChange }
+            aria-describedby="email-help"
+            required/>
+          <p id="email-help" className="sr-only">Saisissez votre email</p>
+          <p className={`error-message ${emailError ? "visible" : ""}`}>{ emailError }</p>
+
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            name="password"
+            value={ password }
+            onChange={(e) => setPassword(e.target.value)}
+            aria-describedby="password-help"
+            required/>
+          <p id="password-help" className="sr-only">Saisissez votre mot de passe</p>
+
+          <div id="login-button">
+            <button
+              type="submit"
+              aria-label="Bouton de connexion à la documentation Swagger"
+              disabled={ !email || !password }
+              aria-disabled={ !email || !password }
+              aria-busy={ isLoading }>Se connecter
+            </button>
+          </div>
+
+        </form>
+      </div>
+      {/* {error && <p className="error-message">{error}</p>} */}
+    </main>
   );
 };
 
 export default LoginPage;
-
