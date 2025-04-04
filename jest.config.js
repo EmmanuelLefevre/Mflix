c5dfe37fdcf56abfd5c12de60f9c9e95745b402e
Emmanuel Lefevre <waiasiaa@protonmail.com>
2025-04-04 15:55:40 +0200

    update config

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,17 @@
+require('dotenv').config({ path: '.env.test' });
+
+/** @type {import('jest').Config} */
+
 module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
   moduleNameMapper: {
-    "^@/(.*)$": "<rootDir>/src/$1",
+    '^@/(.*)$': '<rootDir>/$1',
   },
-  testMatch: ["**/tests/**/*.test.ts"],
-  transform: {
-    "^.+\\.ts$": "ts-jest"
-  },
+  testMatch: ["**/tests/**/*.spec.ts"],
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  testPathIgnorePatterns: ['/node_modules/', '/.next/'],
   globals: {
     "ts-jest": {
       tsconfig: "tsconfig.json"
