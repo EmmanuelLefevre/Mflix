be3b84805805b850e21a790e0fc5e4d53b4d7159
Emmanuel Lefevre <waiasiaa@protonmail.com>
2025-04-04 17:25:40 +0200

    backup

--- jest.setup.js
+++ jest.setup.js
@@ -1,3 +1,20 @@
-process.env.JWT_SECRET = "test_jwt_secret";
-process.env.REFRESH_SECRET = "test_refresh_secret";
-process.env.NODE_ENV = "test";
+// process.env.JWT_SECRET = "test_jwt_secret";
+// process.env.REFRESH_SECRET = "test_refresh_secret";
+// process.env.NODE_ENV = "test";
+
+// const { MongoMemoryServer } = require('mongodb-memory-server');
+// const mongoose = require('mongoose');
+
+
+// let mongoServer;
+// beforeAll(async () => {
+//   mongoServer = await MongoMemoryServer.create();
+//   const mongoUri = mongoServer.getUri();
+//   await mongoose.connect(mongoUri, { useNewUrlParser: true, useUnifiedTopology: true });
+// });
+
+// afterAll(async () => {
+//   await mongoose.disconnect();
+//   await mongoServer.stop();
+// });
+
