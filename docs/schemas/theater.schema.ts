ccfc673f90a8077d313f7751ad1fc05054fe05e0
Emmanuel Lefevre <waiasiaa@protonmail.com>
2025-04-05 15:41:59 +0200

    fix schemas structure error

--- docs/schemas/theater.schema.ts
+++ docs/schemas/theater.schema.ts
@@ -1,57 +1,55 @@
 export const TheaterSchema = {
-  Theater: {
-    type: "object",
-    properties: {
-      _id: {
-        type: "string",
-        example: "63f5b5fa6e7f16cdd60ab2a9"
-      },
-      theaterId: {
-        type: "integer",
-        example: 1000
-      },
-      location: {
-        type: "object",
-        properties: {
-          address: {
-            type: "object",
-            properties: {
-              street1: {
-                type: "string",
-                example: "340 W Market"
-              },
-              street2: {
-                type: "string",
-                example: "Ste backerstreet"
-              },
-              city: {
-                type: "string",
-                example: "Bloomington"
-              },
-              state: {
-                type: "string",
-                example: "MN"
-              },
-              zipcode: {
-                type: "string",
-                example: "55425"
-              }
+  type: "object",
+  properties: {
+    _id: {
+      type: "string",
+      example: "63f5b5fa6e7f16cdd60ab2a9"
+    },
+    theaterId: {
+      type: "integer",
+      example: 1000
+    },
+    location: {
+      type: "object",
+      properties: {
+        address: {
+          type: "object",
+          properties: {
+            street1: {
+              type: "string",
+              example: "340 W Market"
+            },
+            street2: {
+              type: "string",
+              example: "Ste backerstreet"
+            },
+            city: {
+              type: "string",
+              example: "Bloomington"
+            },
+            state: {
+              type: "string",
+              example: "MN"
+            },
+            zipcode: {
+              type: "string",
+              example: "55425"
             }
-          },
-          geo: {
-            type: "object",
-            properties: {
-              type: {
-                type: "string",
-                example: "Point"
+          }
+        },
+        geo: {
+          type: "object",
+          properties: {
+            type: {
+              type: "string",
+              example: "Point"
+            },
+            coordinates: {
+              type: "array",
+              items: {
+                type: "number"
               },
-              coordinates: {
-                type: "array",
-                items: {
-                  type: "number"
-                },
-                example: [-93.24565, 44.85466]
-              }
+              example: [-93.24565, 44.85466]
             }
           }
         }
--- docs/schemas/user.schema.ts
+++ docs/schemas/user.schema.ts
@@ -1,27 +1,21 @@
 export const UserSchema = {
-  components: {
-    schemas: {
-      User: {
-        type: "object",
-        properties: {
-          _id: {
-            type: "string",
-            example: "63f5b5fa6e7f16cdd60ab2a9"
-          },
-          name: {
-            type: "string",
-            example: "Neo"
-          },
-          email: {
-            type: "string",
-            example: "neo@matrix.com"
-          },
-          password: {
-            type: "string",
-            example: "Matrix1999!"
-          }
-        }
-      }
+  type: "object",
+  properties: {
+    _id: {
+      type: "string",
+      example: "63f5b5fa6e7f16cdd60ab2a9"
+    },
+    name: {
+      type: "string",
+      example: "Neo"
+    },
+    email: {
+      type: "string",
+      example: "neo@matrix.com"
+    },
+    password: {
+      type: "string",
+      example: "Matrix1999!"
     }
   }
 };
