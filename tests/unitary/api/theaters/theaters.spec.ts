9f508485a081390d8948dc2e381d927fccd71042
Emmanuel Lefevre <waiasiaa@protonmail.com>
2025-04-10 18:20:00 +0200

    get all theaters unitary tests

--- tests/unitary/api/theaters/theaters.spec.ts
+++ tests/unitary/api/theaters/theaters.spec.ts
@@ -103,4 +103,68 @@ describe('GET /api/theaters', () => {
     expect(body.data).toEqual([]);
     expect(body.message).toBe('No theaters found');
   });
+
+  it('return 200 with theaters when no query paramaters are provided', async () => {
+    (MongoDBSingleton.getDbInstance as jest.Mock).mockResolvedValue(mockDbGetAll);
+    (checkCollectionExists as jest.Mock).mockResolvedValue(true);
+
+    const fakeTheaters = [
+      {
+        _id: "59a47286cfa9a3a73e51e72d",
+        theaterId: 2025,
+        location: {
+          address: {
+            street1: "340 W Market",
+            street2: "Ste backerstreet",
+            city: "Bloomington",
+            state: "MN",
+            zipcode: "55425"
+          },
+          geo: {
+            type: "Point",
+            coordinates: [
+              -93.24565,
+              44.85466
+            ]
+          }
+        }
+      },
+      {
+        _id: "59a47286cfa9a3a73e51e72e",
+        theaterId: 1789,
+        location: {
+          address: {
+            street1: "1621 E Monte Vista Ave",
+            city: "Vacaville",
+            state: "CA",
+            zipcode: "55425"
+          },
+          geo: {
+            type: "Point",
+            coordinates: [
+              -121.96328,
+              38.367649
+            ]
+          }
+        }
+      }
+    ];
+
+    mockDbGetAll.toArray.mockResolvedValue(fakeTheaters);
+
+    const req = {
+      method: 'GET',
+      url: 'http://localhost/api/movies'
+    } as unknown as NextRequest;
+
+    const res = await GET(req);
+    const body = await res.json();
+
+    expect(res.status).toBe(200);
+    expect(body.status).toBe(200);
+    expect(body.data).toEqual(fakeTheaters);
+
+    expect(mockDbGetAll.skip).toHaveBeenCalledWith(0);
+    expect(mockDbGetAll.limit).toHaveBeenCalledWith(10);
+  });
 });
