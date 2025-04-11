ce0b21271a3c5669a2fa59c3cef32e84b8524e5a
Emmanuel Lefevre <waiasiaa@protonmail.com>
2025-04-11 09:06:06 +0200

    Units tests for post a theater

--- tests/unitary/api/theaters/theaters.spec.ts
+++ tests/unitary/api/theaters/theaters.spec.ts
@@ -271,13 +271,13 @@ describe('POST /api/theaters', () => {
     jest.clearAllMocks();
   });
 
-  const buildRequest = (body: object, method: string = 'POST') =>
+  const buildRequest = (body: object | null, method: string = 'POST') =>
     ({
       method,
       json: async () => body,
     }) as unknown as NextRequest;
 
-  it("return 201 the created theater", async () => {
+  it("return 201 with the created theater", async () => {
     (MongoDBSingleton.getDbInstance as jest.Mock).mockResolvedValue(mockDbPost);
     (checkCollectionExists as jest.Mock).mockResolvedValue(true);
     collectionMock.findOne.mockResolvedValue(null);
@@ -301,5 +301,129 @@ describe('POST /api/theaters', () => {
       theaterId: 4,
       location: { address: '123, rue des champs', city: 'Cazaux', state: 'IDF' }
     }));
+  });
+
+  it('return 400 if body is missing', async () => {
+    const req = buildRequest(null);
+
+    const res = await POST(req);
+    const json = await res.json();
+
+    expect(res.status).toBe(400);
+    expect(json.status).toBe(400);
+    expect(json.error).toBe('Request body is required and must be an object');
+  });
+
+  it('return 400 if location is missing', async () => {
+    const req = buildRequest({});
+
+    const res = await POST(req);
+    const json = await res.json();
+
+    expect(res.status).toBe(400);
+    expect(json.status).toBe(400);
+    expect(json.error).toBe('Location is required and must be an object');
+  });
+
+  it('return 400 if body is not an object', async () => {
+    const req = buildRequest("fake string" as unknown as object);
+
+    const res = await POST(req);
+    const json = await res.json();
+
+    expect(res.status).toBe(400);
+    expect(json.status).toBe(400);
+    expect(json.error).toBe('Request body is required and must be an object');
+  });
+
+  it('return 400 if location is not an object', async () => {
+    const req = buildRequest({ location: 'string au lieu d\'objet' });
+
+    const res = await POST(req);
+    const json = await res.json();
+
+    expect(res.status).toBe(400);
+    expect(json.status).toBe(400);
+    expect(json.error).toBe('Location is required and must be an object');
+  });
+
+  it("return 404 if collection 'theaters' doesn't exists", async () => {
+    (MongoDBSingleton.getDbInstance as jest.Mock).mockResolvedValue(mockDbPost);
+    (checkCollectionExists as jest.Mock).mockResolvedValue(false);
+
+    const req = buildRequest({ location: { address: 'X', city: 'Y', state: 'Z' } });
+
+    const res = await POST(req);
+    const json = await res.json();
+
+    expect(res.status).toBe(404);
+    expect(json.status).toBe(404);
+    expect(json.error).toBe("Collection 'theaters' not found");
+  });
+
+  it("return 405 if method is not allowed", async () => {
+    const req = buildRequest({}, 'GET');
+
+    const res = await POST(req);
+    const json = await res.json();
+
+    expect(res.status).toBe(405);
+    expect(json.status).toBe(405);
+    expect(json.error).toBe('Method Not Allowed');
+  });
+
+  it('return 409 if theater already exists', async () => {
+    (MongoDBSingleton.getDbInstance as jest.Mock).mockResolvedValue(mockDbPost);
+    (checkCollectionExists as jest.Mock).mockResolvedValue(true);
+    collectionMock.findOne.mockResolvedValue({ theaterId: 99 });
+
+    const req = buildRequest({ location: { address: '13, rue de la chance', city: 'Cavignac', state: 'Aquitaine' } });
+
+    const res = await POST(req);
+    const json = await res.json();
+
+    expect(res.status).toBe(409);
+    expect(json.status).toBe(409);
+    expect(json.error).toBe('Theater already exists');
+  });
+
+  it("return 500 with Error instance", async () => {
+    (MongoDBSingleton.getDbInstance as jest.Mock).mockRejectedValue(new Error('Error instance'));
+
+    const body = JSON.stringify({ location: { address: '13, rue de la chance', city: 'Cavignac', state: 'Aquitaine' } });
+
+    const req = new Request('http://localhost/api/movies', {
+      method: 'POST',
+      body,
+      headers: { 'Content-Type': 'application/json' }
+    }) as unknown as NextRequest;
+
+    const res = await POST(req);
+    const json = await res.json();
+
+    expect(res.status).toBe(500);
+    expect(json.status).toBe(500);
+    expect(json.error).toBe('Error instance');
+  });
+
+  it("return 500 in case of unknown error", async () => {
+    (MongoDBSingleton.getDbInstance as jest.Mock).mockImplementation(() => {
+      throw 'Error in string and not instance of Error';
+    });
+
+    const body = JSON.stringify({ location: { address: '13, rue de la chance', city: 'Cavignac', state: 'Aquitaine' } });
+
+    const req = new Request('http://localhost/api/movies', {
+      method: 'POST',
+      body,
+      headers: { 'Content-Type': 'application/json' }
+    }) as unknown as NextRequest;
+
+    const res = await POST(req);
+    const json = await res.json();
+
+    expect(res.status).toBe(500);
+    expect(json.status).toBe(500);
+    expect(json.error).toBe('Unknown error occurred');
   });
 });
